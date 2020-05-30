d4a56a3d7deff7e738ea2d86fa0d4a557d71af6d
Konstantin V. Borisov <smeagol74@gmail.com>
2020-05-30 17:06:25 +0700

    * fix resource bundle encoding

--- src/main/java/ru/kvb74/utils/CombinedResourceBundle.java
+++ src/main/java/ru/kvb74/utils/CombinedResourceBundle.java
@@ -1,44 +1,50 @@
 package ru.kvb74.utils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.logging.Logger;
 
 public class CombinedResourceBundle extends ResourceBundle {
 
-    private static Logger log = Logger.getLogger(CombinedResourceBundle.class.getName());
+	private static Logger log = Logger.getLogger(CombinedResourceBundle.class.getName());
 
-    private Map<String, String> combinedResources = new HashMap<>();
-    private List<String> bundleNames;
+	private static final boolean utf8Convert = "1.8".equals(System.getProperty("java.specification.version"));
 
-    public CombinedResourceBundle(List<String> bundleNames) {
-        this.bundleNames = bundleNames;
-    }
+	private Map<String, String> combinedResources = new HashMap<>();
+	private List<String> bundleNames;
 
-    public void load(boolean raiseErrors) {
-        bundleNames.forEach(bundleName ->
-        {
-            try {
-                ResourceBundle bundle = ResourceBundle.getBundle(bundleName);
-                Enumeration<String> keysEnumeration = bundle.getKeys();
-                ArrayList<String> keysList = Collections.list(keysEnumeration);
-                keysList.forEach(key -> combinedResources.put(key, bundle.getString(key)));
-            } catch (Exception ex) {
-                if (raiseErrors) {
-                    throw(ex);
-                } else {
-                    log.warning(ex.getMessage());
-                }
-            }
-        });
-    }
+	public CombinedResourceBundle(List<String> bundleNames) {
+		this.bundleNames = bundleNames;
+	}
 
-    @Override
-    public Object handleGetObject(String key) {
-        return combinedResources.get(key);
-    }
+	public void load(boolean raiseErrors) {
+		bundleNames.forEach(bundleName ->
+		{
+			try {
+				ResourceBundle bundle = ResourceBundle.getBundle(bundleName);
+				Enumeration<String> keysEnumeration = bundle.getKeys();
+				ArrayList<String> keysList = Collections.list(keysEnumeration);
+				keysList.forEach(key -> {
+					String val = bundle.getString(key);
+					combinedResources.put(key, utf8Convert ? new String(val.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8) : val);
+				});
+			} catch (Exception ex) {
+				if (raiseErrors) {
+					throw (ex);
+				} else {
+					log.warning(ex.getMessage());
+				}
+			}
+		});
+	}
 
-    @Override
-    public Enumeration<String> getKeys() {
-        return Collections.enumeration(combinedResources.keySet());
-    }
+	@Override
+	public Object handleGetObject(String key) {
+		return combinedResources.get(key);
+	}
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return Collections.enumeration(combinedResources.keySet());
+	}
 }
